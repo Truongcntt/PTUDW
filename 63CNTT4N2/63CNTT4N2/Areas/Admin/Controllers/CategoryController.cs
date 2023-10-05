e0ac4d122bdd5603202cb98dbdc7c135fd4d8062
Truongcntt <145674099+Truongcntt@users.noreply.github.com>
2023-10-05 19:53:07 +0700

    63.CNTT4N2 trang Index của Category

---
+++ 63CNTT4N2/63CNTT4N2/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyClass.Model;
+using MyClass.DAO;
+
+namespace _63CNTT4N2.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoriesDAO categoriesDAO = new CategoriesDAO();
+       
+        /// ///////////////////////////////////////////////////
+        // GET: Admin/Category
+        public ActionResult Index()
+        {
+            return View(categoriesDAO.getList("Index"));
+        }
+
+        //// GET: Admin/Category/Details/5
+        //public async Task<ActionResult> Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Categories categories = await db.Categories.FindAsync(id);
+        //    if (categories == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(categories);
+        //}
+
+        //// GET: Admin/Category/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: Admin/Category/Create
+        //// To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> Create([Bind(Include = "Id,Name,Slug,ParenId,Order,MetaDesc,MetaKey,CreateBy,CreateAt,UpdateBy,UpdateAt,Status")] Categories categories)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Categories.Add(categories);
+        //        await db.SaveChangesAsync();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View(categories);
+        //}
+
+        //// GET: Admin/Category/Edit/5
+        //public async Task<ActionResult> Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Categories categories = await db.Categories.FindAsync(id);
+        //    if (categories == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(categories);
+        //}
+
+        //// POST: Admin/Category/Edit/5
+        //// To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> Edit([Bind(Include = "Id,Name,Slug,ParenId,Order,MetaDesc,MetaKey,CreateBy,CreateAt,UpdateBy,UpdateAt,Status")] Categories categories)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(categories).State = EntityState.Modified;
+        //        await db.SaveChangesAsync();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(categories);
+        //}
+        ///// ///////////////////////////////////////////////////
+        //// GET: Admin/Category/Delete/5
+        //public async Task<ActionResult> Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Categories categories = await db.Categories.FindAsync(id);
+        //    if (categories == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(categories);
+        //}
+
+        //// POST: Admin/Category/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> DeleteConfirmed(int id)
+        //{
+        //    Categories categories = await db.Categories.FindAsync(id);
+        //    db.Categories.Remove(categories);
+        //    await db.SaveChangesAsync();
+        //    return RedirectToAction("Index");
+        //}
+    }
+}
